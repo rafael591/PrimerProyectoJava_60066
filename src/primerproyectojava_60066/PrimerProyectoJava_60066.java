6737f959ba76e25dc83382ea50f99ed91917e4b0
Rafael Moreno <Rafael Moreno@LAPTOP-ROGBR6G0>
2021-02-27 20:48:38 -0600

    tarea1

---
+++ src/primerproyectojava_60066/PrimerProyectoJava_60066.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerproyectojava_60066;
+
+/**
+ *
+ * @author Rafael Moreno
+ */
+public class PrimerProyectoJava_60066 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int variable_1 = 1; 
+        char variable_2 = 'A';
+        boolean variable_3 = true;
+        
+        System.out.println("variable_1 = " + variable_1);
+        System.out.println("variable_2 = " + variable_2);
+        System.out.println("variable_3 = " + variable_3);
+                
+    }
+    
+}
